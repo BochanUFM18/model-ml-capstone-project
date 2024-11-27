b6b43d7b10247f2ed1e83008ad9432a90b30966c
umar <c206b4ky4376@bangkit.academy>
2024-11-27 22:01:26 +0800

    ini commit ke 12

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, request, jsonify
-import tensorflow as tf  # Sesuaikan dengan library yang Anda gunakan
 
+from flask import Flask
+
+# Membuat objek Flask
 app = Flask(__name__)
 
-# Load model ML
-model = tf.keras.models.load_model('D:\mode-ml\ML_NutriCheck\model\NutriCheck_model.h5')  # Sesuaikan dengan format model Anda
+# Route utama untuk halaman beranda
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    # Preprocessing input data sesuai dengan model Anda
-    input_data = data['input']
-    prediction = model.predict(input_data)
-    return jsonify({'prediction': prediction.tolist()})
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+# Menjalankan aplikasi
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=3000)
 
