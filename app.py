cd7f3998ea0b3eb4e64e1371d41ffffdc3fb60ee
umar <c206b4ky4376@bangkit.academy>
2024-11-27 19:17:43 +0800

    deploy ke 6

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def predict():
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
